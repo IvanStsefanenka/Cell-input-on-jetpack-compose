a08461a7a282aa4181ae3a2654a242555ac2e0fc
IvanStsefanenka <ivan.stsefanenka@slicelife.com>
2021-11-14 15:38:44 +0300

    implemented simplest input in cells

--- app/src/main/java/com/ivanstef/cellinput/MainActivity.kt
+++ app/src/main/java/com/ivanstef/cellinput/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.ivanstef.cellinput
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import com.ivanstef.cellinput.foundation.actionSecondary
-import com.ivanstef.cellinput.foundation.content
-import com.ivanstef.cellinput.widget.input.SegmentedTextInput
 
 class MainActivity : ComponentActivity() {
 
@@ -20,11 +23,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Scaffold(
-                    topBar = { MainTopBar() }
-                ) {
-                    MainContent()
-                }
+                MainContent()
             }
         }
     }
@@ -38,26 +37,45 @@ fun MainContent() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        SegmentedTextInput(
-            cellsAmount = 6,
-            modifier = Modifier.padding(horizontal = 12.dp),
-            unfocusedBackgroundColor = actionSecondary,
-            cellColors = TextFieldDefaults.outlinedTextFieldColors(
-                cursorColor = content,
-                textColor = content,
-                focusedBorderColor = content,
-                unfocusedBorderColor = actionSecondary,
-            ),
-            onFilled = {
-                Log.d("CODE", "Result: $it")
-            }
-        )
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            InputCell(modifier = Modifier.padding(start = 12.dp))
+            InputCell(modifier = Modifier.padding(start = 12.dp))
+        }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun MainTopBar(){
-    TopAppBar {
-        Text(text = "Segmented input")
-    }
+fun InputCell(modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+
+    OutlinedTextField(
+        modifier = modifier
+            .width(56.dp)
+            .height(60.dp),
+        value = text,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Next
+        ),
+        keyboardActions = KeyboardActions(
+            onNext = { focusManager.moveFocus(FocusDirection.Right) }
+        ),
+        onValueChange = {
+            when (it.length) {
+                1 -> {
+                    text = it
+                    focusManager.moveFocus(FocusDirection.Right)
+                }
+                0 -> {
+                    text = it
+                    focusManager.moveFocus(FocusDirection.Left)
+                }
+            }
+        }
+    )
 }
